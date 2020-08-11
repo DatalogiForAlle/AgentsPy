de2b32bc3ef6dd2bbc1cd3b0c009b53bfca786de
jenskanstruplarsen <jensklmail@yahoo.dk>
2020-08-11 13:17:11 +0200

    Files to test reading cell-data from file

--- examples/stupid/generate_cell.py
+++ examples/stupid/generate_cell.py
@@ -1,9 +1,12 @@
 import random
 
 f = open("stupid.cell", "w")
+f.write("This file contains cell data for the StupidModel.\n")
+f.write("The data specifies the rate of food production for each cell.\n")
+f.write("x" + '\t' + "y" + '\t' + "prod" + '\n')
 for y in range(112):
     for x in range(250):
-        f.write(str(x) + " "
-                + str(y) + " "
+        f.write(str(x) + '\t'
+                + str(y) + '\t'
                 + str(random.random()*0.02) + '\n')
 f.close()
