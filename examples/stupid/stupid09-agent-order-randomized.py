18be8b36c708416dbd40d91d29cfa9893d2fa6ce
Martin Dybdal <dybber@dybber.dk>
2020-08-20 19:18:17 +0200

    Simplification of stupid model code (still on TODO: stupid12-16)

--- examples/stupid/stupid09-agent-order-randomized.py
+++ examples/stupid/stupid09-agent-order-randomized.py
@@ -4,82 +4,104 @@ from agents import Agent, Model, run
 
 
 class Bug(Agent):
-    def draw_color(self):
+    def size_to_color(self):
         gradient = max(0, 255-255*self.grow_size/10)
         self.color = (255, gradient, gradient)
 
     def setup(self, model):
         self.size = 8
         self.grow_size = 1
-        self.draw_color()
+        self.size_to_color()
         self.align()
         self.update_current_tile()
 
-    def step(self, model):
-        # Eat from the current tile
-        t = self.current_tile()
-        self.grow_size += min(model["max_food_eat"], t.info["food"])
-        t.info["food"] = max(0, t.info["food"]-model["max_food_eat"])
-        if self.grow_size > 100:
-            model["stop"] = True
-
-        # Find all nearby valid tiles
+    def move(self):
+        """
+        Jump to a random tile in the neighborhood of the agent, which is
+        not occupied by other agents
+        """
+        # Find all nearby tiles within distance of 4 tiles
         nearby_tiles = self.nearby_tiles(-4, -4, 4, 4)
         random.shuffle(nearby_tiles)
 
-        def is_valid_tile(t):
-            return len(t.get_agents()) == 0
-        nearby_tiles = list(filter(is_valid_tile, nearby_tiles))
+        # Find first unoccipied tile
+        new_tile = None
+        for tile in nearby_tiles:
+            if len(tile.get_agents()) == 0:
+                new_tile = tile
 
-        # If there is a valid tile, pick the "first" one and jump to it
-        if len(nearby_tiles) > 0:
-            # Just use the first one in the list, it is shuffled anyways
-            new_t = nearby_tiles[0]
-            self.jump_to(new_t.x*model.width/model.x_tiles,
-                         new_t.y*model.height/model.y_tiles)
-            self.align()
-        self.draw_color()
+        # Jump there
+        if new_tile is not None:
+            self.jump_to_tile(new_tile)
+
+        # Does nothing, if all tiles are occupied
+
+    def eat(self, model):
+        # Eat from the current tile
+        tile = self.current_tile()
+        self.grow_size += min(model["max_food_eat"], tile.info["food"])
+        tile.info["food"] = max(0, tile.info["food"]-model["max_food_eat"])
+        self.size_to_color()
+
+    def step(self, model):
+        self.eat(model)
+        self.move()
+        if self.grow_size > 100:
+            model.pause()
 
 
 def setup(model):
+    # Open data file for writing
     global f
     f = open("stupid.data", "w")
+
     model.reset()
-    people = set([Bug() for i in range(math.floor(model["initial_bugs"]))])
-    model["stop"] = False
-    model.add_agents(people)
-    for t in model.tiles:
-        t.info["food"] = 0.0
-        t.color = (0, 0, 0)
+
+    # Add agents
+    for i in range(int(model["initial_bugs"])):
+        model.add_agent(Bug())
+
+    # Initialize tiles
+    for tile in model.tiles:
+        tile.info["food"] = 0.0
+        tile.color = (0, 0, 0)
 
 
 def step(model):
     global f
-    if not model["stop"]:
-        bug_min = None
-        bug_mean = 0
-        bug_max = None
-        for a in model.agents_random():
-            a.step(model)
-            if not bug_min or bug_min > a.grow_size:
-                bug_min = a.grow_size
-            if not bug_max or bug_max < a.grow_size:
-                bug_max = a.grow_size
-            bug_mean += a.grow_size
-        bug_mean /= model["initial_bugs"]
-        f.write(str(bug_min) + " " + str(bug_mean) + " " + str(bug_max) + "\n")
-        f.flush() # Flush is necessary as long as we can't call f.close()
-                  # when the user exits the program
 
-        for t in model.tiles:
-            food_prod = random.random() * model["max_food_prod"]
-            t.info["food"] += food_prod
-            c = min(255, math.floor(t.info["food"] * 255))
-            t.color = (c, c, c)
-        model.update_plots()
+    # Food production
+    for tile in model.tiles:
+        food_prod = random.uniform(0, model["max_food_prod"])
+        tile.info["food"] += food_prod
+        c = min(255, math.floor(tile.info["food"] * 255))
+        tile.color = (c, c, c)
+
+    # Move all agents
+    for agent in model.agents_random():
+        agent.step(model)
+
+    # Calculate min, average and max bug size
+    bug_min = 100
+    bug_sum = 0
+    bug_max = 0
+    for agent in model.agents:
+        bug_min = min(bug_min, agent.grow_size)
+        bug_max = max(bug_max, agent.grow_size)
+        bug_sum += agent.grow_size
+    bug_mean = bug_sum / len(model.agents)
+
+    # Write min, average and max bug size to file
+    f.write(str(bug_min) + " " + str(bug_mean) + " " + str(bug_max) + "\n")
+    # Flush is necessary as long as we can't call f.close()
+    # when the user exits the program
+    f.flush()
+
+    # Update plots
+    model.update_plots()
 
 
-stupid_model = Model("StupidModel w. randomized agent order (stupid09)",
+stupid_model = Model("StupidModel w. file output (stupid08)",
                      100, 100, tile_size=5)
 stupid_model.add_button("setup", setup)
 stupid_model.add_button("step", step)
@@ -90,5 +112,5 @@ stupid_model.add_controller_row()
 stupid_model.add_slider("max_food_eat", 0.1, 1.0, 1.0)
 stupid_model.add_controller_row()
 stupid_model.add_slider("max_food_prod", 0.01, 0.1, 0.01)
-stupid_model.histogram_bins("grow_size", 0, 10, 5, (0, 0, 0))
+stupid_model.histogram("grow_size", 0, 10, 5, (0, 0, 0))
 run(stupid_model)
