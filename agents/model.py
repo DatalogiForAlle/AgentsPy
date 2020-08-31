7c0f2f4e63f2972d654cba283b8490c80f51d854
jenskanstruplarsen <jensklmail@yahoo.dk>
2020-08-31 10:54:49 +0200

    Revert "Changing model.tiles to list of lists"
    This reverts commit 20418af9e2b62f0d928af4ef9d4710db228baff8.

--- agents/model.py
+++ agents/model.py
@@ -160,7 +160,8 @@ class Agent:
             math.floor(self.__model.y_tiles * self.y / self.__model.height)
             % self.__model.y_tiles
         )
-        return self.__model.tiles[x][y]
+        i = y * self.__model.x_tiles + x
+        return self.__model.tiles[i]
 
     # Returns the surrounding tiles as a 3x3 grid. Includes the current tile.
     def neighbor_tiles(self):
@@ -169,11 +170,9 @@ class Agent:
     def nearby_tiles(self, x1, y1, x2, y2):
         t = self.__current_tile
         tiles = []
-        for x in range(x1,x2+1):
-            for y in range(y1, y2 + 1):
-                tiles += (self.__model.tiles
-                          [(t.x + x) % self.__model.x_tiles]
-                          [(t.y + y) % self.__model.y_tiles])
+        for y in range(y1, y2 + 1):
+            row = self.__model.x_tiles * ((t.y + y) % self.__model.y_tiles)
+            tiles += self.__model.tiles[(row + t.x + x1):(row + t.x + x2 + 1)]
         return tiles
 
     def is_destroyed(self):
@@ -296,8 +295,8 @@ class Model:
             self.height = y_tiles * tile_size
 
             # Initial tileset (empty).
-            self.tiles = [[Tile(x, y, self)
-                           for y in range(y_tiles)]
+            self.tiles = [Tile(x, y, self)
+                          for y in range(y_tiles)
                           for x in range(x_tiles)]
         else:
             cell_data = open(cell_data_file, "r")
@@ -328,8 +327,8 @@ class Model:
             self.__agents = set()
 
             # Initial tileset (empty).
-            self.tiles = [[Tile(x, y, self)
-                           for y in range(y_tiles)]
+            self.tiles = [Tile(x, y, self)
+                          for y in range(y_tiles)
                           for x in range(x_tiles)]
 
             cell_data.close()
@@ -377,8 +376,9 @@ class Model:
         self.__agents = []
         for x in range(self.x_tiles):
             for y in range(self.y_tiles):
-                self.tiles[x][y].color = (0, 0, 0)
-                self.tiles[x][y].info = {}
+                i = y * self.x_tiles + x
+                self.tiles[i].color = (0, 0, 0)
+                self.tiles[i].info = {}
         self.unpause()
 
     def reload(self):
@@ -387,7 +387,7 @@ class Model:
             y = int(tile_data[1])
             for i in range(2, len(tile_data)):
                 variable = self.header_info[i]
-                tile = self.tiles[x][y]
+                tile = self.tiles[y*self.x_tiles+x]
                 tile.info[variable] = float(tile_data[i])
 
     def update_plots(self):
