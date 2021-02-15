a3f16e54d19de4cf2727d6ace3c217b1e6fc5710
jenskanstruplarsen <jensklmail@yahoo.dk>
2021-02-15 09:29:21 +0100

    Small adjustments

--- examples/epidemic_vaccine.py
+++ examples/epidemic_vaccine.py
@@ -29,13 +29,21 @@ class Person(Agent):
         self.infection_level = 600
 
     def turn_immune(self, model):
-        if self.category == 1:
-            model.Infectious -= 1
-        else:
-            model.Susceptible -= 1
+        model.Infectious -= 1
         model.Recovered += 1
         self.color = (0, 0, 200)
         self.category = 2
+
+    def vaccinate(self, model):
+        if self.category == 0:
+            model.Susceptible -= 1
+        elif self.category == 1:
+            model.Infectious -= 1
+        else:
+            model.Recovered -= 1
+        model.Vaccinated += 1
+        self.color = (100, 100, 200)
+        self.category = 3
 
 
 def model_setup(model):
@@ -43,6 +51,7 @@ def model_setup(model):
     model.Susceptible = 0
     model.Infectious = 0
     model.Recovered = 0
+    model.Vaccinated = 0
     model.Vaccine_timer = 0
     model.Vaccine_rate = 0.2
     for person in range(100):
@@ -57,8 +66,9 @@ def model_step(model):
     if model.Vaccine_timer > 30 / model.Vaccine_rate:
         model.Vaccine_timer = 0
         for person in model.agents:
-            if person.category == 0:
-                person.turn_immune(model)
+            # If not infected or already vaccinated
+            if person.category != 1 or person.category != 3:
+                person.vaccinate(model)
                 break
 
     model.update_plots()
@@ -71,8 +81,8 @@ epidemic_model.add_button("Go", model_step, toggle=True)
 epidemic_model.add_slider("Vaccine_rate", 0.2, 0.1, 1.0)
 
 epidemic_model.line_chart(
-    ["Susceptible", "Infectious", "Recovered"], [(0, 200, 0), (200, 0, 0), (0, 0, 200)]
+    ["Susceptible", "Infectious", "Recovered", "Vaccinated"], [(0, 200, 0), (200, 0, 0), (0, 0, 200), (100, 100, 200)]
 )
-epidemic_model.bar_chart(["Susceptible", "Infectious", "Recovered"], (200, 200, 200))
+epidemic_model.bar_chart(["Susceptible", "Infectious", "Recovered", "Vaccinated"], (200, 200, 200))
 
 run(epidemic_model)
